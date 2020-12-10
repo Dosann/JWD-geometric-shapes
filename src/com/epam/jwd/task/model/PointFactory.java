7a86ac1ec9a468662d16946785058e0de41021d6
Dosann <anndosova@gmail.com>
2020-12-10 19:41:36 +0300

    separate factory from point cache

--- src/com/epam/jwd/task/model/PointFactory.java
+++ src/com/epam/jwd/task/model/PointFactory.java
@@ -2,10 +2,18 @@ package com.epam.jwd.task.model;
 
 public class PointFactory {
 
+    private static class PointFactoryHolder {
+        private final static PointFactory instance = new PointFactory();
+    }
+
+    public static PointFactory getInstance() {
+        return PointFactory.PointFactoryHolder.instance;
+    }
+
     private static final Point[] createdPoints = new Point[42];
     private static int amountOfPoints = 0;
 
-    public static Point buildPoint(double x, double y){
+    public Point buildPoint(double x, double y){
         final Point point = new Point(x, y);
         createdPoints[amountOfPoints++] = point;
         return point;
