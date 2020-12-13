6b9849eda7179d1a0633ddd9f771eb81fe3278aa
Dosann <anndosova@gmail.com>
2020-12-13 21:12:56 +0300

    final commits

--- src/com/epam/jwd/task/service/impl/FigureExistencePostProcessor.java
+++ src/com/epam/jwd/task/service/impl/FigureExistencePostProcessor.java
@@ -2,9 +2,6 @@ package com.epam.jwd.task.service.impl;
 
 import com.epam.jwd.task.exception.FigureException;
 import com.epam.jwd.task.exception.FigureNotExistException;
-import com.epam.jwd.task.logic.MultiAngleFigureLogic;
-import com.epam.jwd.task.logic.SquareLogic;
-import com.epam.jwd.task.logic.TriangleLogic;
 import com.epam.jwd.task.model.MultiAngleFigure;
 import com.epam.jwd.task.model.Square;
 import com.epam.jwd.task.model.Triangle;
@@ -17,35 +14,20 @@ public class FigureExistencePostProcessor implements FigurePostProcessor {
     public Figure process(Figure figure) throws FigureException {
         if (figure instanceof Triangle) {
             figure.setTriangleStrategy();
-            checkTriangleValidation(figure);
+            checkFigureValidation(figure);
         } else if (figure instanceof Square) {
             figure.setSquareStrategy();
-            checkSquareValidation(figure);
+            checkFigureValidation(figure);
         } else if (figure instanceof MultiAngleFigure) {
             figure.setMultiAngleStrategy();
-            checkMultiAngleValidation(figure);
+            checkFigureValidation(figure);
         }
         return figure;
     }
 
-    private static void checkTriangleValidation(Figure figure) throws FigureException {
-        Triangle triangle = (Triangle) figure;
-        if (!TriangleLogic.isExist(triangle)) {
-            throw new FigureNotExistException("Triangle doesn't exist");
-        }
-    }
-
-    private static void checkSquareValidation(Figure figure) throws FigureException {
-        Square square = (Square) figure;
-        if (!SquareLogic.isExist(square)) {
-            throw new FigureNotExistException("Square doesn't exist");
-        }
-    }
-
-    private static void checkMultiAngleValidation(Figure figure) throws FigureException {
-        MultiAngleFigure multiAngleFigure = (MultiAngleFigure) figure;
-        if (!MultiAngleFigureLogic.isExist(multiAngleFigure)) {
-            throw new FigureNotExistException("MultiAngle doesn't exist");
+    private void checkFigureValidation(Figure figure) throws FigureException {
+        if (!figure.isFigureExist()) {
+            throw new FigureNotExistException("Figure doesn't exist");
         }
     }
 }
