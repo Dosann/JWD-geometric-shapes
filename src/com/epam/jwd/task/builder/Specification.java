b141942b8755c85bc78d49fdb6edca9600b37a09
Dosann <anndosova@gmail.com>
2020-12-18 11:24:45 +0300

    resolve logic with Class<T>

--- src/com/epam/jwd/task/builder/Specification.java
+++ src/com/epam/jwd/task/builder/Specification.java
@@ -10,6 +10,17 @@ public class Specification {
     private final double area;
     private final double perimeter;
 
+    private int nameIndex;
+    private int areaIndex;
+    private int perimeterIndex;
+
+    private Specification setUpIndex(int nameIndex, int areaIndex, int perimeterIndex) {
+        this.nameIndex = nameIndex;
+        this.areaIndex = areaIndex;
+        this.perimeterIndex = perimeterIndex;
+        return this;
+    }
+
     public Specification(Color color, String name, double area, double perimeter) {
         this.color = color;
         this.name = name;
@@ -33,6 +44,18 @@ public class Specification {
         return perimeter;
     }
 
+    public int getNameIndex() {
+        return nameIndex;
+    }
+
+    public int getAreaIndex() {
+        return areaIndex;
+    }
+
+    public int getPerimeterIndex() {
+        return perimeterIndex;
+    }
+
     @Override
     public String toString() {
         return "Specification{" +
@@ -53,66 +76,69 @@ public class Specification {
         private double area;
         private double perimeter;
 
-        public Builder color(Color color) {
-            this.color =  color;
-            return this;
-        }
-
-        public Builder name(String name) {
-            this.name =  name;
-            return this;
-        }
-
-        public Builder area(double area) {
-            this.area =  area;
-            return this;
-        }
-
-        public Builder perimeter(double perimeter) {
-            this.perimeter =  perimeter;
-            return this;
-        }
-
-        public Specification build() {
-            return new Specification(this.color, this.name, this.area, this.perimeter);
-        }
-
-        //todo: think of logic
-        public <T extends Figure> Builder find(Class<T> figureClass) {
-            name = figureClass.getName();
-            return this;
-        }
+        private int nameIndex;
+        private int areaIndex;
+        private int perimeterIndex;
 
         public Builder withColor(Color color) {
             this.color =  color;
             return this;
         }
 
-        public Builder withNameStartsWith(String name) {
+        public Builder withNameEqualsTo(String name) {
             this.name =  name;
             return this;
         }
 
-        public Builder withAreaGreaterThan(double area) {
+        public Builder withAreaEqualsTo(double area) {
             this.area =  area;
             return this;
+        }
+
+        public Builder withPerimeterEqualsTo(double perimeter) {
+            this.perimeter =  perimeter;
+            return this;
+        }
+
+        public Specification build() {
+            return new Specification(this.color, this.name, this.area, this.perimeter)
+                    .setUpIndex(this.nameIndex, this.areaIndex, this.perimeterIndex);
+        }
+
+        //todo: think of logic
+        public <T extends Figure> Builder find(Class<T> figureClass) {
+//            name = figureClass.getName();
+            return this;
+        }
+
+        public Builder withNameStartsWith(String name) {
+           this.nameIndex = 1;
+            return withNameEqualsTo(name);
+        }
+
+        public Builder withNameEndsWith(String name) {
+            this.nameIndex = -1;
+            return withNameEqualsTo(name);
+        }
+
+        public Builder withAreaGreaterThan(double area) {
+            this.areaIndex = 1;
+            return withAreaEqualsTo(area);
         }
 
         public Builder withAreaLessThen(double area) {
-            this.area =  area;
-            return this;
+            this.areaIndex = -1;
+            return withAreaEqualsTo(area);
         }
 
         public Builder withPerimeterGreaterThan(double perimeter) {
-            this.perimeter =  perimeter;
-            return this;
+            this.perimeterIndex = 1;
+            return withPerimeterEqualsTo(perimeter);
         }
 
         public Builder withPerimeterLessThan(double perimeter) {
-            this.perimeter =  perimeter;
-            return this;
+           this.perimeterIndex = -1;
+           return withPerimeterEqualsTo(perimeter);
         }
-
-
     }
 }
